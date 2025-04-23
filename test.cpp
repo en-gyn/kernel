25c73e0fcecf500783a40ed2820b6b0aea713870
en-gyn <1192523808@qq.com>
2025-04-23 17:55:38 +0800

    1

---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by qi liu on 25-4-23.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,16 @@
+//
+// Created by qi liu on 25-4-23.
+//
+
+#ifndef TEST_H
+#define TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //TEST_H
